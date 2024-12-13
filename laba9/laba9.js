0015b97e75fa47831c5c0df41d5eda0495e430ff
merlisste <58188397+merlisscen@users.noreply.github.com>
2024-12-13 17:08:44 +0300

    Update laba9.js

--- laba9/laba9.js
+++ laba9/laba9.js
@@ -1,2 +1,2 @@
-let result = Math.pow(Math.pow(x, w) - Math.pow(y, 4) - Math.pow(w, x / y), 2);
+let result = (Math.pow(w, 2) * Math.pow(y, 4) + w)/(Math.E - x) + (Math.pow(y, 2) - w)/(Math.pow(x, 4) * Math.pow(x, 3));
 console.log(result);
